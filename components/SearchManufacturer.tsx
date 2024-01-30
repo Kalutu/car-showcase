78e3825c881bac81c95c8ff355a1113eab7d7aab
Kalutu <kalutudaniel@gmail.com>
2024-01-30 11:06:22 +0300

    update combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -24,7 +24,7 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufucturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-fulls">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -61,7 +61,24 @@ const SearchManufacturer = ({
                   }
                   value={item}
                 >
-                  {item}
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
                 </Combobox.Option>
               ))}
             </Combobox.Options>
